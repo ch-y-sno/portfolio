f95860d6779af65ebe03be2924d03dbc45496635
ch-y-sno <yumi@aonaginonotobukkukonpyuta.local>
2024-09-26 14:16:40 +0900

    flashメッセージの設定(success/fail/その他)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def flash_color(type)
+    case type.to_sym
+    when :success then "text-green-800 bg-green-50 dark:text-green-400"
+    when :danger then "text-red-800 bg-red-50 dark:text-red-400"
+    else "text-yellow-800 bg-yellow-50 dark:text-yellow-400"
+    end
+  end
 end
