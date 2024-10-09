5c19a7ac3f8acb996364993360d0a809b0e8c44c
ch-y-sno <yumi@aonaginonotobukkukonpyuta.local>
2024-10-09 15:36:48 +0900

    コメント投稿・削除機能のajax化

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,12 @@
 class CommentsController < ApplicationController
   def create
-    comment = current_user.comments.build(comment_params)
-    if comment.save
-      redirect_to article_path(comment.article), success: t("defaults.flash_message.created", item: Comment.model_name.human)
-    else
-      redirect_to article_path(comment.article), danger: t("defaults.flash_message.not_created", item: Comment.model_name.human)
-    end
+    @comment = current_user.comments.build(comment_params)
+    @comment.save
+  end
+
+  def destroy
+    @comment = current_user.comments.find(params[:id])
+    @comment.destroy!
   end
 
   private
