49cc00fbd722c0713f5eb58f708d180b7d6cce9f
ch-y-sno <yumi@aonaginonotobukkukonpyuta.local>
2024-10-24 12:33:06 +0900

    お題一覧表示・お題削除機能(ユーザートップページ)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,5 +5,7 @@ class HomeController < ApplicationController
   def index
     @article = Article.new
     @articles = Article.includes(:user).order(created_at: :desc)
+    @topics = current_user.topics.order(created_at: :desc)
+    @team = current_user.team
   end
 end
