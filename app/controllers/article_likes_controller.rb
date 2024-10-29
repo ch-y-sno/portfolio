6d2e5359f3bb35a5c14cb3ed2184b1957e4f5974
ch-y-sno <yumi@aonaginonotobukkukonpyuta.local>
2024-10-29 15:09:56 +0900

    投稿へのいいね機能のajax化

--- app/controllers/article_likes_controller.rb
+++ app/controllers/article_likes_controller.rb
@@ -1,15 +1,11 @@
 class ArticleLikesController < ApplicationController
   def create
-    article = Article.find(params[:article_id])
-    current_user.article_like(article)
-    @team = Team.find(params[:id])
-    redirect_to team_path(@team), success: t(".success")
+    @article = Article.find(params[:article_id])
+    current_user.article_like(@article)
   end
 
   def destroy
-    article = current_user.article_likes.find(params[:id]).article
-    current_user.article_unlike(article)
-    @team = Team.find(params[:id])
-    redirect_to team_path(@team), success: t(".success"), status: :see_other
+    @article = current_user.article_likes.find(params[:id]).article
+    current_user.article_unlike(@article)
   end
 end
